2ae5945cc103d4f7cc019c74919a0695c60b83f5
zeyad-belal <zeyadbelal00@gmail.com>
2025-08-13 14:38:44 +0300

    generate product by id

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -6,16 +6,15 @@ faker.seed(123);
 const TOTAL = 10_000;
 let START = 0;
 
-
-const pageCache = new Map();      // `${page}:${pageSize}` -> { items, pagination }
-const productMap = new Map();     // id -> product (for O(1) get by id)
+const pageCache = new Map(); // `${page}:${pageSize}` -> { items, pagination }
+const productMap = new Map(); // id -> product (for O(1) get by id)
 
 const key = (page, pageSize) => `${page}:${pageSize}`;
 
 function generatePage(page, pageSize) {
   const items = Array.from({ length: pageSize }, () => {
     const id = faker.string.uuid();
-    const p ={
+    const p = {
       id,
       name: faker.commerce.productName(),
       price: Number(faker.commerce.price({ min: 5, max: 500, dec: 2 })),
@@ -23,9 +22,9 @@ function generatePage(page, pageSize) {
       image: `/product.jpg`,
       description: faker.commerce.productDescription(),
     };
-    
+
     productMap.set(id, p);
-    return p
+    return p;
   });
 
   return {
@@ -46,13 +45,31 @@ export async function getProductsPage(page, pageSize) {
 
   const data = generatePage(page, pageSize);
   pageCache.set(k, data);
-  console.log('pageCache',pageCache)
+  console.log("pageCache", pageCache);
   return data;
 }
 
+export function generateProductFromId(id) {
+  faker.seed(123 + id);
+  return {
+    id,
+    name: faker.commerce.productName(),
+    price: Number(faker.commerce.price({ min: 5, max: 500, dec: 2 })),
+    thumbnail: "/product.jpg",
+    image: "/product.jpg",
+    description: faker.commerce.productDescription(),
+  };
+}
 
-// O(1) 
 export function getProductById(id) {
   if (!id) return null;
-  return productMap.get(id) || null;
+
+  let product = productMap.get(id); // O(1)
+
+  // incase user refreshed the product details page ( special logic for the fak api generation)
+  if (!product) {
+    product = generateProductFromId(id);
+  }
+
+  return product;
 }
